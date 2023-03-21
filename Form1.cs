12258c2f414b5f412e1ccfffc994bb8f0efa3cd6
palis-ss <54706343+palis-ss@users.noreply.github.com>
2023-03-21 17:14:36 +0700

    reorganize source code and add comments

--- Form1.cs
+++ Form1.cs
@@ -13,6 +13,20 @@ namespace MedCartTest
             InitializeComponent();
         }
 
+        // Init GUI
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+
+            foreach (string port in ports)
+            {
+                cbPort.Items.Add(port);
+            }
+
+            cbPort.SelectedIndex = 0;
+        }
+
+        // User clicks on the Connect button
         private void btnConnect_Click(object sender, EventArgs e)
         {
             try
@@ -28,19 +42,21 @@ namespace MedCartTest
                 }
                 else
                 {
-                    modbus.SerialPort = cbPort.Text;
+                    // set serial port parameters
+                    modbus.SerialPort = cbPort.Text;  // read port name from CB
                     modbus.Baudrate = 19200;
                     modbus.Parity = Parity.None;
                     modbus.StopBits = StopBits.One;
                     modbus.UnitIdentifier = 1;  // slave address
-                    modbus.NumberOfRetries = 2;
+                    modbus.NumberOfRetries = 2;  // set no of retries so it's won't take too long to respond
+                    
                     modbus.Connect();
 
                     btnConnect.Text = "Disconnect";
                     cbPort.Enabled = false;
 
-                    // Example how to read to register (read board and firmware version)                    
-                    var regs = modbus.ReadInputRegisters(40, 2);
+                    // Example how to read registers (read board and firmware version)
+                    int[] regs = modbus.ReadInputRegisters(40, 2);  // Read input register address 40, 2 words
                     byte[] ver0 = BitConverter.GetBytes(regs[0]);
                     byte[] ver1 = BitConverter.GetBytes(regs[1]);
                     string boardver = ver0[1].ToString() + "." + ver0[0].ToString();
@@ -54,18 +70,7 @@ namespace MedCartTest
             }
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            string[] ports = SerialPort.GetPortNames();
-
-            foreach (string port in ports)
-            {
-                cbPort.Items.Add(port);
-            }
-
-            cbPort.SelectedIndex = 0;
-        }
-
+        // Refresh serial port list when CB is clicked
         private void cbPort_Click(object sender, EventArgs e)
         {
             string[] ports = SerialPort.GetPortNames();
@@ -79,6 +84,7 @@ namespace MedCartTest
             cbPort.SelectedIndex = 0;
         }
 
+        // Close serial port before exit
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             if (modbus.Connected)
@@ -88,8 +94,7 @@ namespace MedCartTest
             }
         }
 
-
-        // Example how to write to register (open drawer solenoid)
+        // Example how to write to a holding register (open drawer solenoid)
         private void btnOpenDrawer_Click(object sender, EventArgs e)
         {
             try
@@ -97,7 +102,7 @@ namespace MedCartTest
                 if (modbus.Connected)
                 {
                     ushort drawerno = decimal.ToUInt16(numSpinner1.Value);
-                    modbus.WriteSingleRegister(1, (ushort)(1 << (drawerno - 1)));
+                    modbus.WriteSingleRegister(1, (ushort)(1 << (drawerno - 1)));  // Write a holding register address 1
                 }
                 else
                     MessageBox.Show("Port not open");
