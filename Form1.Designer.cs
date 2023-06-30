2f8fc0a8cf5b6ae774435e44d41fd9afc9218030
palis-ss <54706343+palis-ss@users.noreply.github.com>
2023-06-30 18:30:56 +0700

    add power supply control

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,14 +30,14 @@
         {
             components = new System.ComponentModel.Container();
             label1 = new Label();
-            btnConnect = new Button();
-            cbPort = new ComboBox();
-            lbInfo = new Label();
+            btnMCConnect = new Button();
+            cbMCPort = new ComboBox();
+            lbHWInfo = new Label();
             groupBox1 = new GroupBox();
+            lblSensorInfo = new Label();
             btnOpenDrawer = new Button();
             numSpinner1 = new NumericUpDown();
             label2 = new Label();
-            txtVoltage = new TextBox();
             lblBattInfo = new Label();
             timer1 = new System.Windows.Forms.Timer(components);
             btnSendVolt = new Button();
@@ -45,56 +45,76 @@
             lblBattRecords = new Label();
             btnClearRecords = new Button();
             chkEnableParam = new CheckBox();
+            groupBox2 = new GroupBox();
+            btnSupplyPortRefresh = new Button();
+            btnAutoRecord = new Button();
+            btnSupplyConnect = new Button();
+            cbSupplyPort = new ComboBox();
+            label3 = new Label();
+            spnVoltage = new NumericUpDown();
+            btnMCPortRefresh = new Button();
             groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)numSpinner1).BeginInit();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)spnVoltage).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(87, 54);
+            label1.Location = new Point(41, 54);
             label1.Name = "label1";
-            label1.Size = new Size(60, 15);
+            label1.Size = new Size(106, 15);
             label1.TabIndex = 5;
-            label1.Text = "Serial Port";
+            label1.Text = "MetCart Serial Port";
             // 
-            // btnConnect
+            // btnMCConnect
             // 
-            btnConnect.Location = new Point(300, 50);
-            btnConnect.Name = "btnConnect";
-            btnConnect.Size = new Size(75, 23);
-            btnConnect.TabIndex = 3;
-            btnConnect.Text = "Connect";
-            btnConnect.UseVisualStyleBackColor = true;
-            btnConnect.Click += btnConnect_Click;
+            btnMCConnect.Location = new Point(300, 50);
+            btnMCConnect.Name = "btnMCConnect";
+            btnMCConnect.Size = new Size(75, 23);
+            btnMCConnect.TabIndex = 3;
+            btnMCConnect.Text = "Connect";
+            btnMCConnect.UseVisualStyleBackColor = true;
+            btnMCConnect.Click += btnConnect_Click;
             // 
-            // cbPort
+            // cbMCPort
             // 
-            cbPort.FormattingEnabled = true;
-            cbPort.Location = new Point(153, 50);
-            cbPort.Name = "cbPort";
-            cbPort.Size = new Size(121, 23);
-            cbPort.TabIndex = 4;
-            cbPort.Click += cbPort_Click;
+            cbMCPort.FormattingEnabled = true;
+            cbMCPort.Location = new Point(153, 50);
+            cbMCPort.Name = "cbMCPort";
+            cbMCPort.Size = new Size(121, 23);
+            cbMCPort.TabIndex = 4;
+            cbMCPort.Click += cbPort_Click;
             // 
-            // lbInfo
+            // lbHWInfo
             // 
-            lbInfo.AutoSize = true;
-            lbInfo.Location = new Point(17, 31);
-            lbInfo.Name = "lbInfo";
-            lbInfo.Size = new Size(12, 15);
-            lbInfo.TabIndex = 6;
-            lbInfo.Text = "-";
+            lbHWInfo.AutoSize = true;
+            lbHWInfo.Location = new Point(17, 31);
+            lbHWInfo.Name = "lbHWInfo";
+            lbHWInfo.Size = new Size(12, 15);
+            lbHWInfo.TabIndex = 6;
+            lbHWInfo.Text = "-";
             // 
             // groupBox1
             // 
-            groupBox1.Controls.Add(lbInfo);
+            groupBox1.Controls.Add(lblSensorInfo);
+            groupBox1.Controls.Add(lbHWInfo);
             groupBox1.Location = new Point(87, 107);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(314, 86);
+            groupBox1.Size = new Size(314, 132);
             groupBox1.TabIndex = 7;
             groupBox1.TabStop = false;
-            groupBox1.Text = "Version info";
+            groupBox1.Text = "Info";
+            // 
+            // lblSensorInfo
+            // 
+            lblSensorInfo.AutoSize = true;
+            lblSensorInfo.Location = new Point(17, 80);
+            lblSensorInfo.Name = "lblSensorInfo";
+            lblSensorInfo.Size = new Size(12, 15);
+            lblSensorInfo.TabIndex = 7;
+            lblSensorInfo.Text = "-";
             // 
             // btnOpenDrawer
             // 
@@ -108,11 +128,12 @@
             // 
             // numSpinner1
             // 
-            numSpinner1.Location = new Point(184, 270);
-            numSpinner1.Maximum = new decimal(new int[] { 10, 0, 0, 0 });
+            numSpinner1.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            numSpinner1.Location = new Point(176, 258);
+            numSpinner1.Maximum = new decimal(new int[] { 12, 0, 0, 0 });
             numSpinner1.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
             numSpinner1.Name = "numSpinner1";
-            numSpinner1.Size = new Size(40, 23);
+            numSpinner1.Size = new Size(57, 36);
             numSpinner1.TabIndex = 9;
             numSpinner1.Value = new decimal(new int[] { 1, 0, 0, 0 });
             // 
@@ -125,17 +146,10 @@
             label2.TabIndex = 10;
             label2.Text = "Drawer No.";
             // 
-            // txtVoltage
-            // 
-            txtVoltage.Location = new Point(557, 75);
-            txtVoltage.Name = "txtVoltage";
-            txtVoltage.Size = new Size(100, 23);
-            txtVoltage.TabIndex = 11;
-            // 
             // lblBattInfo
             // 
             lblBattInfo.AutoSize = true;
-            lblBattInfo.Location = new Point(557, 30);
+            lblBattInfo.Location = new Point(30, 29);
             lblBattInfo.Name = "lblBattInfo";
             lblBattInfo.Size = new Size(12, 15);
             lblBattInfo.TabIndex = 12;
@@ -147,7 +161,7 @@
             // 
             // btnSendVolt
             // 
-            btnSendVolt.Location = new Point(684, 63);
+            btnSendVolt.Location = new Point(246, 64);
             btnSendVolt.Name = "btnSendVolt";
             btnSendVolt.Size = new Size(148, 44);
             btnSendVolt.TabIndex = 13;
@@ -157,7 +171,7 @@
             // 
             // btnFetchBattCal
             // 
-            btnFetchBattCal.Location = new Point(557, 125);
+            btnFetchBattCal.Location = new Point(30, 126);
             btnFetchBattCal.Name = "btnFetchBattCal";
             btnFetchBattCal.Size = new Size(148, 40);
             btnFetchBattCal.TabIndex = 14;
@@ -168,7 +182,7 @@
             // lblBattRecords
             // 
             lblBattRecords.AutoSize = true;
-            lblBattRecords.Location = new Point(557, 189);
+            lblBattRecords.Location = new Point(30, 188);
             lblBattRecords.Name = "lblBattRecords";
             lblBattRecords.Size = new Size(12, 15);
             lblBattRecords.TabIndex = 15;
@@ -176,9 +190,9 @@
             // 
             // btnClearRecords
             // 
-            btnClearRecords.Location = new Point(771, 125);
+            btnClearRecords.Location = new Point(246, 126);
             btnClearRecords.Name = "btnClearRecords";
-            btnClearRecords.Size = new Size(109, 40);
+            btnClearRecords.Size = new Size(148, 40);
             btnClearRecords.TabIndex = 16;
             btnClearRecords.Text = "Clear Records";
             btnClearRecords.UseVisualStyleBackColor = true;
@@ -187,7 +201,7 @@
             // chkEnableParam
             // 
             chkEnableParam.AutoSize = true;
-            chkEnableParam.Location = new Point(771, 174);
+            chkEnableParam.Location = new Point(262, 184);
             chkEnableParam.Name = "chkEnableParam";
             chkEnableParam.Size = new Size(118, 19);
             chkEnableParam.TabIndex = 17;
@@ -195,25 +209,111 @@
             chkEnableParam.UseVisualStyleBackColor = true;
             chkEnableParam.CheckedChanged += chkEnableParam_CheckedChanged;
             // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(btnSupplyPortRefresh);
+            groupBox2.Controls.Add(btnAutoRecord);
+            groupBox2.Controls.Add(btnSupplyConnect);
+            groupBox2.Controls.Add(cbSupplyPort);
+            groupBox2.Controls.Add(label3);
+            groupBox2.Controls.Add(spnVoltage);
+            groupBox2.Controls.Add(chkEnableParam);
+            groupBox2.Controls.Add(btnClearRecords);
+            groupBox2.Controls.Add(lblBattRecords);
+            groupBox2.Controls.Add(lblBattInfo);
+            groupBox2.Controls.Add(btnFetchBattCal);
+            groupBox2.Controls.Add(btnSendVolt);
+            groupBox2.Location = new Point(488, 12);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(448, 370);
+            groupBox2.TabIndex = 18;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Battery Operations";
+            // 
+            // btnSupplyPortRefresh
+            // 
+            btnSupplyPortRefresh.Font = new Font("Wingdings 3", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btnSupplyPortRefresh.Location = new Point(413, 275);
+            btnSupplyPortRefresh.Name = "btnSupplyPortRefresh";
+            btnSupplyPortRefresh.Size = new Size(29, 23);
+            btnSupplyPortRefresh.TabIndex = 20;
+            btnSupplyPortRefresh.Text = "Q";
+            btnSupplyPortRefresh.UseVisualStyleBackColor = true;
+            btnSupplyPortRefresh.Click += btnSupplyPortRefresh_Click;
+            // 
+            // btnAutoRecord
+            // 
+            btnAutoRecord.Location = new Point(246, 313);
+            btnAutoRecord.Name = "btnAutoRecord";
+            btnAutoRecord.Size = new Size(148, 40);
+            btnAutoRecord.TabIndex = 20;
+            btnAutoRecord.Text = "Auto Populate Records";
+            btnAutoRecord.UseVisualStyleBackColor = true;
+            btnAutoRecord.Click += btnAutoRecord_Click;
+            // 
+            // btnSupplyConnect
+            // 
+            btnSupplyConnect.Location = new Point(367, 246);
+            btnSupplyConnect.Name = "btnSupplyConnect";
+            btnSupplyConnect.Size = new Size(75, 23);
+            btnSupplyConnect.TabIndex = 19;
+            btnSupplyConnect.Text = "Connect";
+            btnSupplyConnect.UseVisualStyleBackColor = true;
+            btnSupplyConnect.Click += btnSupplyConnect_Click;
+            // 
+            // cbSupplyPort
+            // 
+            cbSupplyPort.FormattingEnabled = true;
+            cbSupplyPort.Location = new Point(227, 246);
+            cbSupplyPort.Name = "cbSupplyPort";
+            cbSupplyPort.Size = new Size(121, 23);
+            cbSupplyPort.TabIndex = 19;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(227, 228);
+            label3.Name = "label3";
+            label3.Size = new Size(99, 15);
+            label3.TabIndex = 19;
+            label3.Text = "Supply Serial Port";
+            // 
+            // spnVoltage
+            // 
+            spnVoltage.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            spnVoltage.Location = new Point(108, 65);
+            spnVoltage.Maximum = new decimal(new int[] { 14, 0, 0, 0 });
+            spnVoltage.Minimum = new decimal(new int[] { 9, 0, 0, 0 });
+            spnVoltage.Name = "spnVoltage";
+            spnVoltage.Size = new Size(120, 36);
+            spnVoltage.TabIndex = 18;
+            spnVoltage.Value = new decimal(new int[] { 9, 0, 0, 0 });
+            // 
+            // btnMCPortRefresh
+            // 
+            btnMCPortRefresh.Font = new Font("Wingdings 3", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            btnMCPortRefresh.Location = new Point(381, 50);
+            btnMCPortRefresh.Name = "btnMCPortRefresh";
+            btnMCPortRefresh.Size = new Size(29, 23);
+            btnMCPortRefresh.TabIndex = 19;
+            btnMCPortRefresh.Text = "Q";
+            btnMCPortRefresh.UseVisualStyleBackColor = true;
+            btnMCPortRefresh.Click += btnMCPortRefresh_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(948, 394);
-            Controls.Add(chkEnableParam);
-            Controls.Add(btnClearRecords);
-            Controls.Add(lblBattRecords);
-            Controls.Add(btnFetchBattCal);
-            Controls.Add(btnSendVolt);
-            Controls.Add(lblBattInfo);
-            Controls.Add(txtVoltage);
+            Controls.Add(btnMCPortRefresh);
             Controls.Add(label2);
             Controls.Add(numSpinner1);
             Controls.Add(btnOpenDrawer);
             Controls.Add(groupBox1);
             Controls.Add(label1);
-            Controls.Add(btnConnect);
-            Controls.Add(cbPort);
+            Controls.Add(btnMCConnect);
+            Controls.Add(cbMCPort);
+            Controls.Add(groupBox2);
             Name = "Form1";
             Text = "MedCart Protocol Test";
             FormClosing += Form1_FormClosing;
@@ -221,6 +321,9 @@
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)numSpinner1).EndInit();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)spnVoltage).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -228,14 +331,13 @@
         #endregion
 
         private Label label1;
-        private Button btnConnect;
-        private ComboBox cbPort;
-        private Label lbInfo;
+        private Button btnMCConnect;
+        private ComboBox cbMCPort;
+        private Label lbHWInfo;
         private GroupBox groupBox1;
         private Button btnOpenDrawer;
         private NumericUpDown numSpinner1;
         private Label label2;
-        private TextBox txtVoltage;
         private Label lblBattInfo;
         private System.Windows.Forms.Timer timer1;
         private Button btnSendVolt;
@@ -243,5 +345,14 @@
         private Label lblBattRecords;
         private Button btnClearRecords;
         private CheckBox chkEnableParam;
+        private GroupBox groupBox2;
+        private NumericUpDown spnVoltage;
+        private Label lblSensorInfo;
+        private Button btnSupplyConnect;
+        private ComboBox cbSupplyPort;
+        private Label label3;
+        private Button btnAutoRecord;
+        private Button btnSupplyPortRefresh;
+        private Button btnMCPortRefresh;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -7,8 +7,10 @@ namespace MedCartTest
 {
     public partial class Form1 : Form
     {
-        static SerialPort serialPort = new();
-        ModbusSerialMaster master = ModbusSerialMaster.CreateRtu(serialPort);  // create modbus master
+        static SerialPort MCSerialPort = new();
+        static SerialPort SupplySerialPort = new();
+        ModbusSerialMaster MCMaster = ModbusSerialMaster.CreateRtu(MCSerialPort);  // create modbus master
+        ModbusSerialMaster SupplyMaster = ModbusSerialMaster.CreateRtu(SupplySerialPort);  // create modbus master
 
         public Form1()
         {
@@ -22,10 +24,12 @@ namespace MedCartTest
 
             foreach (string port in ports)
             {
-                cbPort.Items.Add(port);
+                cbMCPort.Items.Add(port);
+                cbSupplyPort.Items.Add(port);
             }
 
-            cbPort.SelectedIndex = 0;
+            cbMCPort.SelectedIndex = 0;
+            cbSupplyPort.SelectedIndex = 0;
         }
 
         // User clicks on the Connect button
@@ -33,37 +37,39 @@ namespace MedCartTest
         {
             try
             {
-                if (serialPort.IsOpen)
+                if (MCSerialPort.IsOpen)
                 {
-                    serialPort.Close();
+                    MCSerialPort.Close();
 
-                    btnConnect.Text = "Connect";
-                    cbPort.Enabled = true;
+                    btnMCConnect.Text = "Connect";
+                    cbMCPort.Enabled = true;
 
-                    lbInfo.Text = "-";
+                    lbHWInfo.Text = "-";
                     timer1.Enabled = false;
                 }
                 else
                 {
                     // set serial port parameters
-                    serialPort.PortName = cbPort.Text;  // read port name from CB
-                    serialPort.BaudRate = 19200;
-                    serialPort.DataBits = 8;
-                    serialPort.Parity = Parity.None;
-                    serialPort.StopBits = StopBits.One;
+                    MCSerialPort.PortName = cbMCPort.Text;  // read port name from CB
+                    MCSerialPort.BaudRate = 19200;
+                    MCSerialPort.DataBits = 8;
+                    MCSerialPort.Parity = Parity.None;
+                    MCSerialPort.StopBits = StopBits.One;
 
-                    serialPort.Open();
+                    MCSerialPort.Open();
 
-                    btnConnect.Text = "Disconnect";
-                    cbPort.Enabled = false;
+                    btnMCConnect.Text = "Disconnect";
+                    cbMCPort.Enabled = false;
 
                     // Example how to read registers (read board and firmware version)
-                    ushort[] regs = master.ReadInputRegisters(1, 40, 2);  // Read input register address 40, 2 words
+                    ushort[] regs = MCMaster.ReadInputRegisters(1, 40, 2);  // Read input register address 40, 2 words
                     byte[] ver0 = BitConverter.GetBytes(regs[0]);
                     byte[] ver1 = BitConverter.GetBytes(regs[1]);
                     string boardver = ver0[1].ToString() + "." + ver0[0].ToString();
                     string fwver = ver1[1].ToString() + "." + ver1[0].ToString();
-                    lbInfo.Text = "Board: " + boardver + "\nFirmware: " + fwver;
+                    lbHWInfo.Text = "Board: " + boardver + "\nFirmware: " + fwver;
+
+                    FetchBattCal();
 
                     timer1.Enabled = true;
                 }
@@ -79,23 +85,29 @@ namespace MedCartTest
         {
             string[] ports = SerialPort.GetPortNames();
 
-            cbPort.Items.Clear();
+            cbMCPort.Items.Clear();
             foreach (string port in ports)
             {
-                cbPort.Items.Add(port);
+                cbMCPort.Items.Add(port);
             }
 
-            cbPort.SelectedIndex = 0;
+            cbMCPort.SelectedIndex = 0;
         }
 
         // Close serial port before exit
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-            if (serialPort.IsOpen)
+            if (MCSerialPort.IsOpen)
             {
                 timer1.Enabled = false;
                 Debug.WriteLine("Closing serial port before exit");
-                serialPort.Close();
+                MCSerialPort.Close();
+            }
+            if (SupplySerialPort.IsOpen)
+            {
+                timer1.Enabled = false;
+                Debug.WriteLine("Closing serial port before exit");
+                SupplySerialPort.Close();
             }
         }
 
@@ -104,10 +116,10 @@ namespace MedCartTest
         {
             try
             {
-                if (serialPort.IsOpen)
+                if (MCSerialPort.IsOpen)
                 {
                     ushort drawerno = decimal.ToUInt16(numSpinner1.Value);
-                    master.WriteSingleRegister(1, 1, (ushort)(1 << (drawerno - 1)));  // Write a holding register address 1
+                    MCMaster.WriteSingleRegister(1, 1, (ushort)(1 << (drawerno - 1)));  // Write a holding register address 1
                 }
                 else
                     MessageBox.Show("Port not open");
@@ -120,31 +132,59 @@ namespace MedCartTest
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            ushort[] d = master.ReadInputRegisters(1, 52, 1);
+            ushort[] d = MCMaster.ReadInputRegisters(1, 52, 1);
             ushort nibble1 = (ushort)(d[0] >> 8);
             ushort nibble0 = (ushort)(d[0] & 0xFF);
             double vbatt = (double)nibble1 + (double)nibble0 * 0.01;
             lblBattInfo.Text = string.Format("Batt level: {0}V", vbatt.ToString("N2"));
+
+            d = MCMaster.ReadInputRegisters(1, 22, 3);
+            double[] sensor = new double[3];
+            for (int i = 0; i < d.Length; i++)
+            {
+                nibble1 = (ushort)(d[i] >> 8);
+                nibble0 = (ushort)(d[i] & 0xFF);
+                sensor[i] = (double)nibble1 + (double)nibble0 * 0.01;
+
+            }
+            lblSensorInfo.Text = string.Format("Temp: {0}, {1}, {2}",
+                sensor[0].ToString("N2"), sensor[1].ToString("N2"), sensor[2].ToString("N2"));
+
+            d = MCMaster.ReadInputRegisters(1, 32, 3);
+            for (int i = 0; i < d.Length; i++)
+            {
+                nibble1 = (ushort)(d[i] >> 8);
+                nibble0 = (ushort)(d[i] & 0xFF);
+                sensor[i] = (double)nibble1 + (double)nibble0 * 0.01;
+
+            }
+            lblSensorInfo.Text += string.Format("\nHumidity: {0}, {1}, {2}",
+                sensor[0].ToString("N2"), sensor[1].ToString("N2"), sensor[2].ToString("N2"));
+
         }
 
         private void btnSendVolt_Click(object sender, EventArgs e)
         {
-            double.TryParse(txtVoltage.Text, out double value);
-            master.WriteSingleRegister(1, 100, (ushort)(value * 100));
+            MCMaster.WriteSingleRegister(1, 10000, (ushort)(spnVoltage.Value * 100));
 
             System.Threading.Thread.Sleep(100);
-            btnFetchBattCal_Click(sender, e);  // refresh value
+            FetchBattCal();  // refresh value
         }
 
         private void btnFetchBattCal_Click(object sender, EventArgs e)
         {
-            ushort[] d = master.ReadInputRegisters(1, 102, 1);
+            FetchBattCal();
+        }
+
+        void FetchBattCal()
+        { 
+            ushort[] d = MCMaster.ReadInputRegisters(1, 10002, 1);
             lblBattRecords.Text = string.Format("Number of Records: {0}", d[0]);
 
             if (d[0] > 0)
             {
-                ushort[] x = master.ReadHoldingRegisters(1, 110, d[0]);
-                ushort[] y = master.ReadHoldingRegisters(1, 120, d[0]);
+                ushort[] x = MCMaster.ReadHoldingRegisters(1, 10100, d[0]);
+                ushort[] y = MCMaster.ReadHoldingRegisters(1, 10200, d[0]);
 
                 for (int i = 0; i < d[0]; i++)
                 {
@@ -152,19 +192,120 @@ namespace MedCartTest
                 }
             }
 
-            d = master.ReadHoldingRegisters(1, 103, 1);
+            d = MCMaster.ReadHoldingRegisters(1, 10003, 1);
             chkEnableParam.Checked = d[0] > 0;
         }
 
         private void btnClearRecords_Click(object sender, EventArgs e)
         {
-            master.WriteSingleRegister(1, 101, 0);
-            btnFetchBattCal_Click(sender, e);  // refresh value
+            MCMaster.WriteSingleRegister(1, 10001, 0);
+            FetchBattCal();  // refresh value
         }
 
         private void chkEnableParam_CheckedChanged(object sender, EventArgs e)
         {
-            master.WriteSingleRegister(1, 103, chkEnableParam.Checked ? (ushort)1 : (ushort)0);
+            MCMaster.WriteSingleRegister(1, 10003, chkEnableParam.Checked ? (ushort)1 : (ushort)0);
+        }
+
+        private void btnSupplyConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (SupplySerialPort.IsOpen)
+                {
+                    SupplySerialPort.Close();
+
+                    btnSupplyConnect.Text = "Connect";
+                    cbSupplyPort.Enabled = true;
+                }
+                else
+                {
+                    // set serial port parameters
+                    SupplySerialPort.PortName = cbSupplyPort.Text;  // read port name from CB
+                    SupplySerialPort.BaudRate = 9600;
+                    SupplySerialPort.DataBits = 8;
+                    SupplySerialPort.Parity = Parity.None;
+                    SupplySerialPort.StopBits = StopBits.One;
+
+                    SupplySerialPort.Open();
+
+                    btnSupplyConnect.Text = "Disconnect";
+                    cbSupplyPort.Enabled = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnMCPortRefresh_Click(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+
+            cbMCPort.Items.Clear();
+            foreach (string port in ports)
+            {
+                cbMCPort.Items.Add(port);
+            }
+
+            cbMCPort.SelectedIndex = 0;
+        }
+
+        private void btnSupplyPortRefresh_Click(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+
+            cbSupplyPort.Items.Clear();
+            foreach (string port in ports)
+            {
+                cbSupplyPort.Items.Add(port);
+            }
+
+            cbSupplyPort.SelectedIndex = 0;
+        }
+
+        const ushort CHARGER_SET_VOLTAGE_OFFSET = 0;
+        const ushort CHARGER_SET_CURRENT_OFFSET = 1;
+        const ushort CHARGER_READ_VOLTAGE_OFFSET = 2;
+        const ushort CHARGER_READ_CURRENT_OFFSET = 3;
+        const ushort CHARGER_CCCV_OFFSET = 8;
+        const ushort CHARGER_ONOFF_OFFSET = 9;
+        const ushort CHARGER_SET_DEFAULT_VOLTAGE_OFFSET = 0x50;
+        const ushort CHARGER_SET_DEFAULT_CURRENT_OFFSET = 0x51;
+        const ushort CHARGER_SET_OVER_VOLTAGE_OFFSET = 0x52;
+        const ushort CHARGER_SET_OVER_CURRENT_OFFSET = 0x53;
+        private void btnAutoRecord_Click(object sender, EventArgs e)
+        {
+            List<ushort> volts = new List<ushort> {
+                900, 1000, 1100, 1200, 1300};
+
+            if(MCSerialPort.IsOpen == false)
+            {
+                MessageBox.Show("Please connect to target board and try again");
+                return;
+            }
+
+            if (SupplySerialPort.IsOpen == false)
+            {
+                MessageBox.Show("Please connect to power supply and try again");
+                return;
+            }
+
+            lblBattRecords.Text = "Populating records.  Please wait ...";
+
+            // set current limit to 1A
+            SupplyMaster.WriteSingleRegister(1, CHARGER_SET_CURRENT_OFFSET, 100);
+
+            // set voltages and record
+            foreach (ushort vol in volts)
+            {
+                SupplyMaster.WriteSingleRegister(1, CHARGER_SET_VOLTAGE_OFFSET, vol);
+                Thread.Sleep(500);
+                MCMaster.WriteSingleRegister(1, 10000, vol);
+                Thread.Sleep(500);
+            }
+            FetchBattCal();
         }
     }
 }
